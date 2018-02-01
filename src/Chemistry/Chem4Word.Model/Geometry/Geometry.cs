4cba009e1c3e043a49c7a0f1755d7bed7503c859
deadlyvices <clyde.davies@gmail.com>
2018-02-01 16:13:00 +0000

    Tidying up of code

--- src/Chemistry/Chem4Word.Model/Geometry/Geometry.cs
+++ src/Chemistry/Chem4Word.Model/Geometry/Geometry.cs
@@ -299,30 +299,33 @@ namespace Chem4Word.Model.Geometry
         {
             var points = hull.ToArray();
            
-                Path path = new Path()
-                {
-                    Stroke = Brushes.Black,
-                    StrokeThickness = 1
-                };
-                if (points.Length == 0)
-                    return path;
+            Path path = new Path
+            {
+                StrokeThickness = 1
+            };
 
-                PathSegmentCollection pathSegments = new PathSegmentCollection();
-                for (int i = 1; i < points.Length; i++)
-                    pathSegments.Add(new LineSegment(points[i], true));
-
-                path.Data = new PathGeometry()
-                {
-                    Figures = new PathFigureCollection()
-                    {
-                        new PathFigure()
-                        {
-                            StartPoint = points[0],
-                            Segments = pathSegments
-                        }
-                    }
-                };
+            if (points.Length == 0)
+            {
                 return path;
+            }
+            PathSegmentCollection pathSegments = new PathSegmentCollection();
+            for (int i = 1; i < points.Length; i++)
+            {
+                pathSegments.Add(new LineSegment(points[i], true));
+            }
+            path.Data = new PathGeometry
+            {
+                Figures = new PathFigureCollection
+                {
+                    new PathFigure
+                    {
+                        StartPoint = points[0],
+                        Segments = pathSegments
+                    }
+                }
+            };
+
+            return path;
            
         }
     }
