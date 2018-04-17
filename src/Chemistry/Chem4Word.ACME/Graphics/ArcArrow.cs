bd49dfa506829d343cad2f481240938072aa9070
Mike Williams <mike@doublewide.co.uk>
2018-04-17 15:56:38 +0100

    Add headers

--- src/Chemistry/Chem4Word.ACME/Graphics/ArcArrow.cs
+++ src/Chemistry/Chem4Word.ACME/Graphics/ArcArrow.cs
@@ -3,7 +3,8 @@
 //  This software is released under the Apache License, Version 2.0.
 //  The license and further copyright text can be found in the file LICENSE.md
 //  at the root directory of the distribution.
-// -
+// ---------------------------------------------------------------------------
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
