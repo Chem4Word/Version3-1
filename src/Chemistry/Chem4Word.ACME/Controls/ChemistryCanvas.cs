9396607b994f840e0df0101c1b4b375b590801f0
Mike Williams <mike@doublewide.co.uk>
2018-06-04 12:50:42 +0100

    Attempt 1

--- src/Chemistry/Chem4Word.ACME/Controls/ChemistryCanvas.cs
+++ src/Chemistry/Chem4Word.ACME/Controls/ChemistryCanvas.cs
@@ -25,32 +25,5 @@ namespace Chem4Word.ACME.Controls
         public ChemistryCanvas()
         {
         }
-
-        protected override Size MeasureOverride(Size constraint)
-        {
-            Size size = new Size();
-
-            foreach (UIElement element in this.InternalChildren)
-            {
-                double left = Canvas.GetLeft(element);
-                double top = Canvas.GetTop(element);
-                left = double.IsNaN(left) ? 0 : left;
-                top = double.IsNaN(top) ? 0 : top;
-
-                //measure desired size for each child
-                element.Measure(constraint);
-
-                Size desiredSize = element.DesiredSize;
-                if (!double.IsNaN(desiredSize.Width) && !double.IsNaN(desiredSize.Height))
-                {
-                    size.Width = Math.Max(size.Width, left + desiredSize.Width);
-                    size.Height = Math.Max(size.Height, top + desiredSize.Height);
-                }
-            }
-            // add margin
-            size.Width += 10;
-            size.Height += 10;
-            return size;
-        }
     }
 }
