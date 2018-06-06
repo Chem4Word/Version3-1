d0c980f04a9c2246e1b0780ba7f753650f048016
Mike Williams <mike@doublewide.co.uk>
2018-06-06 23:57:37 +0100

    ACME Editor intgrated as Plug-In

---
+++ src/PlugIns/Editors/Chem4Word.Editor.ACME/Editor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using IChem4Word.Contracts;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Forms;
+using Chem4Word.Core.UI.Forms;
+using Chem4Word.Model;
+using Chem4Word.Model.Converters;
+
+namespace Chem4Word.Editor.ACME
+{
+    public class Editor : IChem4WordEditor
+    {
+        private static string _product = Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+        private static string _class = MethodBase.GetCurrentMethod().DeclaringType?.Name;
+
+        public string Name => "ACME Structure Editor";
+
+        public string Description => "This is the brand new editor";
+
+        public bool HasSettings => false;
+
+        public Point TopLeft { get; set; }
+
+        public IChem4WordTelemetry Telemetry { get; set; }
+
+        public string ProductAppDataPath { get; set; }
+
+        public Dictionary<string, string> Properties { get; set; }
+
+        public string Cml { get; set; }
+
+        public bool ChangeSettings(Point topLeft)
+        {
+            // This PlugIn has no settings
+            return false;
+        }
+
+        public void LoadSettings()
+        {
+            // This PlugIn has no settings
+        }
+
+        public DialogResult Edit()
+        {
+            DialogResult result = DialogResult.Cancel;
+
+            string module = $"{_product}.{_class}.{MethodBase.GetCurrentMethod().Name}()";
+            try
+            {
+                Telemetry.Write(module, "Verbose", "Called");
+                if (HasSettings)
+                {
+                    LoadSettings();
+                }
+
+                // Strip off Formulae and ChemicalNames as we don't edit them here
+                CMLConverter cmlConverter = new CMLConverter();
+                Model.Model model = cmlConverter.Import(Cml);
+                foreach (Molecule molecule in model.Molecules)
+                {
+                    molecule.ChemicalNames.Clear();
+                    molecule.Formulas.Clear();
+                    molecule.ConciseFormula = "";
+                }
+
+                EditorHost host = new EditorHost(cmlConverter.Export(model));
+                host.TopLeft = TopLeft;
+                host.ShowDialog();
+                result = host.Result;
+                Cml = host.OutputValue;
+                host.Close();
+                host.Dispose();
+            }
+            catch (Exception ex)
+            {
+                new ReportError(Telemetry, TopLeft, module, ex).ShowDialog();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/PlugIns/Editors/Chem4Word.Editor.ACME/EditorHost.cs
@@ -0,0 +1,76 @@
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2018, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Chem4Word.ACME;
+
+namespace Chem4Word.Editor.ACME
+{
+    public partial class EditorHost : Form
+    {
+        public System.Windows.Point TopLeft { get; set; }
+
+        public Size FormSize { get; set; }
+
+        public DialogResult Result = DialogResult.Cancel;
+
+        public string OutputValue { get; set; }
+
+        public EditorHost(string cml)
+        {
+            InitializeComponent();
+
+            this.MinimumSize = new Size(300, 200);
+
+            Chem4Word.ACME.Editor ec = new Chem4Word.ACME.Editor(cml);
+            ec.InitializeComponent();
+            elementHost1.Child = ec;
+            ec.OnOkButtonClick += OnWpfOkButtonClick;
+        }
+
+        private void OnWpfOkButtonClick(object sender, EventArgs e)
+        {
+            WpfEventArgs args = (WpfEventArgs)e;
+            if (args.Button.Equals("SAVE"))
+            {
+                Result = DialogResult.OK;
+                OutputValue = args.OutputValue;
+                Hide();
+            }
+        }
+
+        private void EditorHost_Load(object sender, EventArgs e)
+        {
+            if (TopLeft.X != 0 && TopLeft.Y != 0)
+            {
+                Left = (int)TopLeft.X;
+                Top = (int)TopLeft.Y;
+            }
+            if (FormSize.Width != 0 && FormSize.Height != 0)
+            {
+                Width = FormSize.Width;
+                Height = FormSize.Height;
+            }
+        }
+
+        private void EditorHost_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Chem4Word.ACME.Editor ec = elementHost1.Child as Chem4Word.ACME.Editor;
+            if (ec.Dirty)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                ec.OnOkButtonClick -= OnWpfOkButtonClick;
+                ec = null;
+            }
+        }
+    }
+}
