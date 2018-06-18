e342f14b9a00620e3e9e789727e12277f92bd716
Mike Williams <mike@doublewide.co.uk>
2018-06-18 22:50:23 +0100

    First Tab is now looking good

--- src/Chem4Word.V3/UI/WPF/SettingsControl.xaml.cs
+++ src/Chem4Word.V3/UI/WPF/SettingsControl.xaml.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Chem4Word.ACME;
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2018, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+using Chem4Word.Core.Helpers;
 using Chem4Word.Core.UI.Wpf;
-using UserControl = System.Windows.Controls.UserControl;
+using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Chem4Word.UI.WPF
 {
@@ -46,6 +43,24 @@ namespace Chem4Word.UI.WPF
             args.OutputValue = "";
 
             OnButtonClick?.Invoke(this, args);
+        }
+
+        private void SettingsControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var imageStream = ResourceHelper.GetBinaryResource(Assembly.GetExecutingAssembly(), "Preferences.png");
+            if (imageStream != null)
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = imageStream;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
+
+                EditorSettingsButtonImage.Source = bitmap;
+                RendererSettingsButtonImage.Source = bitmap;
+                SearcherSettingsButtonImage.Source = bitmap;
+            }
         }
     }
 }
