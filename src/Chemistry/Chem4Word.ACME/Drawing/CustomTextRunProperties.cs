a75d16d091de104e4bb21a9f414e2460dd9567f2
Mike Williams <mike@doublewide.co.uk>
2019-02-18 19:34:39 +0000

    Tying to fix merge conflicts

---
+++ src/Chemistry/Chem4Word.ACME/Drawing/CustomTextRunProperties.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+using System.Windows.Media.TextFormatting;
+
+namespace Chem4Word.ACME.Drawing
+{
+    partial class CustomTextSource
+    {
+        public class CustomTextRunProperties : TextRunProperties
+        {
+            private bool _subscript;
+            public CustomTextRunProperties(bool subscript)
+            {
+                _subscript = subscript;
+            }
+            public override System.Windows.Media.Brush BackgroundBrush
+            {
+                get { return null; }
+            }
+
+            public override CultureInfo CultureInfo
+            {
+                get { return CultureInfo.CurrentCulture; }
+            }
+
+            public override double FontHintingEmSize
+            {
+                get { return GlyphText.SymbolSize; }
+            }
+
+            public override double FontRenderingEmSize
+            {
+                get { return GlyphText.SymbolSize; }
+            }
+
+            public override Brush ForegroundBrush
+            {
+                get { return Brushes.Black; }
+            }
+
+            public override System.Windows.TextDecorationCollection TextDecorations
+            {
+                get { return new System.Windows.TextDecorationCollection(); }
+            }
+
+            public override System.Windows.Media.TextEffectCollection TextEffects
+            {
+                get { return new TextEffectCollection(); }
+            }
+
+            public override System.Windows.Media.Typeface Typeface
+            {
+                get { return GlyphUtils.SymbolTypeface; }
+            }
+
+            public override TextRunTypographyProperties TypographyProperties
+            {
+                get
+                {
+                    return new CustomTextRunTypographyProperties(_subscript);
+                }
+            }
+
+        }
+    }
+}
